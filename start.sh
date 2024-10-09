462b6a9d8955a5c13e5b12b81ad8235e2767ee44
maky <s0ph19803@gmail.com>
2024-10-09 18:58:02 -0400

    added requirements for python

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Start the Node.js application in the background
 node kostrackerr.js &
+
+# Start the Python application
 python kostracker.py
